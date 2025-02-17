9fa7825173f26f80110fbeedc26bb1b17fe81839
Siddhant4742 <siddhantandure@gmail.com>
2025-02-17 23:39:31 +0530

    web scraping using multithreading

--- Indeed/indeed.py
+++ Indeed/indeed.py
@@ -1,11 +1,12 @@
 
+# from Indeed.Indeed_job_scraper import scrape_indeed_jobs
+# from Indeed.Indeed_url_generator import generate_indeed_url_india
+
+
 from Indeed_job_scraper import scrape_indeed_jobs
 from Indeed_url_generator import generate_indeed_url_india
 
-
-# Main function
 if __name__ == "__main__":
-    # Get user input for job role, location, and additional filters
     job_role = input("Enter the job role (e.g., Python Developer): ")
     location = input("Enter the location (e.g., Bengaluru, Karnataka): ")
     
@@ -18,12 +19,28 @@ if __name__ == "__main__":
     if date_posted_filter not in valid_fromage_values:
         print(f"Invalid value entered for date posted. Defaulting to '14' (last two weeks).")
         date_posted_filter = "14"
-    # Generate the Indeed URL for India with additional filters
     target_url = generate_indeed_url_india(job_role, location,  date_posted=date_posted_filter)
     
     print(f"Scraping job listings from: {target_url}")
 
-    # Scrape job listings
+    jobs = scrape_indeed_jobs(target_url)
+
+
+    for job in jobs:
+        if job["Job Title"]!="N/A":
+            print(job)
+            print("-"*50)
+
+# def indeed(job_role,location,date_posted_filter):
+    valid_fromage_values = ["1", "3", "7", "14"]
+    
+    if date_posted_filter not in valid_fromage_values:
+        print(f"Invalid value entered for date posted. Defaulting to '14' (last two weeks).")
+        date_posted_filter = "14"
+    target_url = generate_indeed_url_india(job_role, location,  date_posted_filter)
+        
+    print(f"Scraping job listings from: {target_url}")
+
     jobs = scrape_indeed_jobs(target_url)
 
 
--- Linkedin/linkedin.py
+++ Linkedin/linkedin.py
@@ -1,18 +1,13 @@
-from linkedin_url_generator import generate_linkedin_job_url
-from linkedin_job_fetcher import fetchAndSave
-from linkedin_job_parser import parse_all_job_details
-# Function to parse the HTML file and extract details for multiple jobs
+from Linkedin.linkedin_url_generator import generate_linkedin_job_url
+from Linkedin.linkedin_job_fetcher import fetchAndSave
+from Linkedin.linkedin_job_parser import parse_all_job_details
+
+def linkedin(keyword,location,experience,job_type,remote,date_posted,company,industry):
+    print(" linkedin thread started")
+    url = generate_linkedin_job_url(keyword,location,experience,job_type,remote,date_posted,company,industry)
+    print("Generated LinkedIn URL:", url)
+    file_path = "data/text_3.html"
+    fetchAndSave(url, file_path)
 
 
-
-# Example usage
-# Generate LinkedIn job search URL
-url = generate_linkedin_job_url()
-print("Generated LinkedIn URL:", url)
-file_path = "data/text_3.html"
-fetchAndSave(url, file_path)
-
-
-
-# Call the function to parse and display the details for all job listings
-parse_all_job_details(file_path)
+    parse_all_job_details(file_path)
