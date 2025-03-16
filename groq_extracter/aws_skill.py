56a15a45b80da929a8f64348215d30ec5fdab43a
Siddhant4742 <siddhantandure@gmail.com>
2025-03-16 13:01:42 +0530

    combine testing with integration with website

--- groq_extracter/aws_skill.py
+++ groq_extracter/aws_skill.py
@@ -2,9 +2,10 @@ from flask import Flask, request, jsonify
 from groq import Groq
 from text_extraction import extract_clean_text_from_pdf
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app,resources={r"/*":{"origins":"*"}})
 # Set your API key
 API_KEY = "API_KEY"
 
@@ -20,7 +21,7 @@ def extract_from_pdf():
 
     # Save the file temporarily
     file_path = f"/tmp/{file.filename}"
-    file.save(file_path)
+    file.save(file_path)    
 
     # Extract text
     extracted_text = extract_clean_text_from_pdf(file_path)
@@ -30,11 +31,10 @@ def extract_from_pdf():
 
     # Send request to Groq
     chat_completion = client.chat.completions.create(
-        messages=[
-            {
+    messages=[
+        {
                 "role": "user",
-                "content": extracted_text + " strictly extract skills and location and give a single list which are mentioned in resume",
-            }
+                "content": extracted_text + " strictly extract applicant name, branch, college, most relevant skill and location and give a single list which are mentioned in resume if branch is not mentioned set default to CSE"}
         ],
         model="llama3-70b-8192",
     )
@@ -44,8 +44,8 @@ def extract_from_pdf():
 
     # Remove the temporary file
     os.remove(file_path)
-
-    return jsonify({"extracted_info": output})
+    print(output)
+    return jsonify({"status":"success","extracted_info": output})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
